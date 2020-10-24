839edb096bd87464e9766903e5137be4e0729d6f
IRINA-IONELA BORC <irina.borc@stud.ubbcluj.ro>
2020-10-24 16:27:21 +0300

    i

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 
 namespace Borc_Irina_Lab2
 {
-    /// <summary>
+    
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
